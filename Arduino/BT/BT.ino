9314751cbab63732f4ceb1d44274a8ff39291336
Lucyanno Frota <lucyannofrota@gmail.com>
2021-05-21 19:01:47 +0100

    Arduino Code For Serial Forwarding

---
+++ Arduino/BT/BT.ino
@@ -0,0 +1,29 @@
+
+
+#include <SoftwareSerial.h>
+
+#define ENABLE_PIN 7
+ 
+const int pinoRX = 2;
+const int pinoTX = 3;
+ 
+SoftwareSerial bluetooth(pinoRX, pinoTX);
+ 
+void setup(){
+  pinMode(ENABLE_PIN,OUTPUT); digitalWrite(ENABLE_PIN,LOW);
+  Serial.begin(38400);
+  Serial.println("%-----%");
+  Serial.println("%-----%");
+  bluetooth.begin(38400);
+  bluetooth.println("%-----%");
+  bluetooth.println("%-----%");
+  delay(100);
+}
+
+void loop(){
+  if (bluetooth.available())
+    Serial.write(bluetooth.read());
+ 
+  if (Serial.available())
+    bluetooth.write(Serial.read());
+}
---
+++ Arduino/BT_CONF/BT_CONF.ino
@@ -0,0 +1,110 @@
+#include <SoftwareSerial.h>
+
+/*
+ * Necessario utilizar '?' apos o comando
+AT COMMANDS
+AT : Ceck the connection.
+AT+NAME : See default name
+AT+ADDR : see default address
+AT+VERSION : See version
+AT+UART : See baudrate
+AT+ROLE: See role of bt module(1=master/0=slave)
+AT+RESET : Reset and exit AT mode
+AT+ORGL : Restore factory settings
+AT+PSWD: see default password
+AT+BIND: Master Connect (Slave addr: xxxx,xxx,xx)
+ * 
+ */
+
+/* 
+ * BaudRate 38400
+ * Name BT1, BT2
+ * PSW 1122, 1122
+ */
+#include <stdio.h>
+#include <string.h>
+
+#define ENABLE_PIN 7
+
+
+#define RX_PIN 3 // Voltage Divider 3.3V
+#define TX_PIN 2
+
+SoftwareSerial ATDevice(TX_PIN, RX_PIN); // RX, TX -> Inverted For Module
+
+  String command(const char *toSend, unsigned long milliseconds) {
+    String result;
+    Serial.print("Sending: ");
+    Serial.println(toSend);
+    ATDevice.println(toSend);
+    unsigned long startTime = millis();
+    Serial.print("Received: ");
+    while (millis() - startTime < milliseconds) {
+      if (ATDevice.available()) {
+        char c = ATDevice.read();
+        Serial.write(c);
+        result += c;  // append to the result string
+      }
+    }
+  Serial.println();  // new line after timeout.
+  return result;
+}
+
+String command(const char *toSend) {
+    String result = command(toSend,500);
+    delay(500);
+    return result;
+}
+
+String Rdata;
+
+void setup() {
+
+  Serial.begin(38400);
+  
+//  pinMode(VCC_PIN,OUTPUT); digitalWrite(VCC_PIN,HIGH);
+  pinMode(ENABLE_PIN,OUTPUT); digitalWrite(ENABLE_PIN,HIGH);
+  
+  Serial.println("Sending AT commands...");
+  
+  ATDevice.begin(38400);
+  delay(300);
+  command("AT");
+  Rdata = command("AT+NAME?");
+  if(Rdata.indexOf("BT1") != -1){
+    command("AT+RMAAD");
+    command("AT+PSWD=1234");
+    command("AT+PAIR=0020,10,000240");
+    command("AT+BIND=0020,10,000240");
+    command("AT+LINK=0020,10,000240");
+    command("AT+CMODE=0");
+    command("AT+RESET");
+  }
+  else{
+    command("AT+ADDR?");
+    command("AT+PSWD?");
+    command("AT+RESET");
+  }
+}
+
+
+
+
+
+
+//void readBTCOM(void){
+//  if (ATDevice.available()) Serial.write(ATDevice.read());
+//}
+
+void loop()
+
+{
+if (ATDevice.available())
+
+Serial.write(ATDevice.read());
+
+if (Serial.available())
+
+ATDevice.write(Serial.read());
+
+}
